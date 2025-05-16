954ebd1ff9abc8de59ce63a0f560694689bf4138
Devi Aulya <devi@example.com>
2025-05-16 23:32:02 +0700

    depi

--- resources/views/layouts/pelanggan-master.blade.php
+++ resources/views/layouts/pelanggan-master.blade.php
@@ -49,14 +49,14 @@
         <div class="sidebar-logo">
           <!-- Logo Header -->
           <div class="logo-header" data-background-color="dark">
-            <a href="#" class="logo">
+          <a href="index.html" class="logo">
               <img
-                src="{{ asset('kaiadmin/assets/img/logo-wipaykuu-2.png') }}"
+                src="{{ asset('kaiadmin/assets/img/logo-fdl-white.png') }}"
                 alt="navbar brand"
                 class="navbar-brand"
-                height="20"
-              />Fadillahnet
-            </a>
+                width="100"
+              /> 
+              </a>
             <div class="nav-toggle">
               <button class="btn btn-toggle toggle-sidebar">
                 <i class="gg-menu-right"></i>
