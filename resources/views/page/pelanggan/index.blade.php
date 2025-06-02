af4c57112679b40a070c2bf6fc457c2818659652
Devi Aulya <devi@example.com>
2025-06-02 22:47:41 +0700

    karep

--- resources/views/page/pelanggan/index.blade.php
+++ resources/views/page/pelanggan/index.blade.php
@@ -7,10 +7,10 @@
             <h3 class="fw-bold mb-3">Pelanggan</h3>
             <ul class="breadcrumbs mb-3">
                 <li class="nav-home">
-                    <a href="#"><i class="icon-home"></i></a>
+                    <a href="{{ route('admin.dashboard') }}"><i class="icon-home"></i></a>
                 </li>
                 <li class="separator"><i class="icon-arrow-right"></i></li>
-                <li class="nav-item"><a href="#">Pelanggan</a></li>
+                <li class="nav-item"><a href="{{ route('admin.pelanggan') }}">Pelanggan</a></li>
             </ul>
         </div>
 
--- resources/views/page/pelanggan/show.blade.php
+++ resources/views/page/pelanggan/show.blade.php
@@ -6,11 +6,11 @@
         <div class="page-header">
             <h3 class="fw-bold mb-3">Detail Pelanggan</h3>
             <ul class="breadcrumbs mb-3">
-                <li class="nav-home"><a href="#"><i class="icon-home"></i></a></li>
+                <li class="nav-home"><a href="{{ route('admin.dashboard') }}"><i class="icon-home"></i></a></li>
                 <li class="separator"><i class="icon-arrow-right"></i></li>
-                <li class="nav-item"><a href="#">Pelanggan</a></li>
+                <li class="nav-item"><a href="{{ route('admin.pelanggan') }}">Pelanggan</a></li>
                 <li class="separator"><i class="icon-arrow-right"></i></li>
-                <li class="nav-item"><a href="#">Detail</a></li>
+                <li class="nav-item"><a href="{{ route('admin.pelanggan.show', $pelanggan->id_pelanggan) }}">Detail</a></li>
             </ul>
         </div>
 
