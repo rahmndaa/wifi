988340ed639a0789a19119f34927f70f3af58875
Devi Aulya <devi@example.com>
2025-09-23 01:37:18 -0500

    Perbaikan periode exel

--- app/Exports/PembayaranExport.php
+++ app/Exports/PembayaranExport.php
@@ -8,9 +8,20 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class PembayaranExport implements FromCollection, WithHeadings
 {
+    protected $tahun;
+    protected $bulan;
+    protected $status;
+
+    public function __construct($tahun = null, $bulan = null, $status = null)
+    {
+        $this->tahun = $tahun;
+        $this->bulan = $bulan;
+        $this->status = $status;
+    }
+
     public function collection()
     {
-        return DB::table('pembayaran')
+        $query = DB::table('pembayaran')
             ->join('tagihan', 'tagihan.id_tagihan', '=', 'pembayaran.id_tagihan')
             ->join('pelanggan', 'pelanggan.id_pelanggan', '=', 'tagihan.id_pelanggan')
             ->select(
@@ -22,8 +33,24 @@ class PembayaranExport implements FromCollection, WithHeadings
                 'pembayaran.metode_pembayaran',
                 'pembayaran.tanggal_bayar'
             )
-            ->orderByDesc('pembayaran.tanggal_bayar')
-            ->get();
+            ->orderByDesc('pembayaran.tanggal_bayar');
+
+        // Filter tahun jika ada
+        if ($this->tahun) {
+            $query->where('tagihan.periode_tahun', $this->tahun);
+        }
+
+        // Filter bulan jika ada
+        if ($this->bulan) {
+            $query->where('tagihan.periode_bulan', $this->bulan);
+        }
+
+        // Filter status jika ada
+        if ($this->status) {
+            $query->where('tagihan.status', $this->status);
+        }
+
+        return $query->get();
     }
 
     public function headings(): array
@@ -39,3 +66,4 @@ class PembayaranExport implements FromCollection, WithHeadings
         ];
     }
 }
+
--- resources/views/page/pembayaran/index.blade.php
+++ resources/views/page/pembayaran/index.blade.php
@@ -24,10 +24,14 @@
 
     
     {{-- Kanan: Tombol Export --}}
-    <div>
-        <a href="{{ route('pembayaran.export') }}" class="btn btn-success btn-sm">Export</a>
+  <div>
+  <a href="{{ route('pembayaran.export', [
+      'tahun' => request('tahun'),
+      'bulan' => request('bulan'),
+      'status' => request('status')
+  ]) }}" class="btn btn-success btn-sm">Export</a>
+  </div>
 
-    </div>
 </div>
 
 
