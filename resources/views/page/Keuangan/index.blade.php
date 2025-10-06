5dc6413062d2d2c95ebacf08e7d3428a8b112324
Aulianabila <abilaaaull@gmail.com>
2025-10-06 23:58:41 +0700

    update warna

--- resources/views/page/Keuangan/index.blade.php
+++ resources/views/page/Keuangan/index.blade.php
@@ -14,34 +14,35 @@
             </ul>
         </div>
 
-    {{-- Filter Periode --}}
-    <form action="{{ route('admin.keuangan.index') }}" method="GET" class="mb-4">
-        <div class="row mb-3">
-            <div class="col-md-2">
-                <label class="form-label">Dari</label>
-                <input type="date" name="dari" class="form-control" value="{{ request('dari') }}">
-            </div>
-            <div class="col-md-2">
-                <label class="form-label">Sampai</label>
-                <input type="date" name="sampai" class="form-control" value="{{ request('sampai') }}">
-            </div>
-            <div class="col-md-1 d-grid">
-                <label class="form-label">&nbsp;</label>
-                <button type="submit" class="btn btn-primary btn-sm">
-                    <i class="fa fa-search"></i>
-                </button>
-            </div>
+        {{-- Filter Periode --}}
+        <form action="{{ route('admin.keuangan.index') }}" method="GET" class="mb-4">
+            <div class="row mb-3">
+                <div class="col-md-2">
+                    <label class="form-label">Dari</label>
+                    <input type="date" name="dari" class="form-control" value="{{ request('dari') }}">
+                </div>
+                <div class="col-md-2">
+                    <label class="form-label">Sampai</label>
+                    <input type="date" name="sampai" class="form-control" value="{{ request('sampai') }}">
+                </div>
+                <div class="col-md-1 d-grid">
+                    <label class="form-label">&nbsp;</label>
+                    <button type="submit" class="btn btn-primary btn-sm">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
 
-            <div class="col-md-1 d-grid">
-             <label class="form-label">&nbsp;</label>
-                <a href="{{ route('admin.keuangan.export', ['dari' => request('dari'), 'sampai' => request('sampai')]) }}" class="btn btn-success btn-sm">Export
-                </a>
+                <div class="col-md-1 d-grid">
+                    <label class="form-label">&nbsp;</label>
+                    <a href="{{ route('admin.keuangan.export', ['dari' => request('dari'), 'sampai' => request('sampai')]) }}" class="btn btn-success btn-sm">
+                        Export
+                    </a>
+                </div>
             </div>
-        </div>
-    </form>
+        </form>
 
-     <div class="row">
-            {{-- Ringkasan --}}
+        <div class="row">
+            {{-- Total Pemasukan --}}
             <div class="col-md-4">
                 <div class="card card-stats card-round">
                     <div class="card-body">
@@ -62,6 +63,7 @@
                 </div>
             </div>
 
+            {{-- Total Pengeluaran --}}
             <div class="col-md-4">
                 <div class="card card-stats card-round">
                     <div class="card-body">
@@ -82,21 +84,33 @@
                 </div>
             </div>
 
+            {{-- Balance dengan Warna Dinamis --}}
+            @php
+                $balance = $totalPemasukan - $totalPengeluaran;
+            @endphp
+
             <div class="col-md-4">
-                <div class="card card-stats card-round">
+                <div class="card card-stats card-round 
+                    @if($balance >= 0) bg-success text-white shadow-lg 
+                    @else bg-danger text-white shadow-lg 
+                    @endif">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-5">
                                 <div class="icon-big text-center">
-                                    <i class="fas fa-balance-scale text-primary"></i>
+                                    <i class="fas fa-balance-scale text-white"></i>
                                 </div>
                             </div>
                             <div class="col-7 col-stats">
                                 <div class="numbers">
-                                    <p class="card-category">Balance</p>
-                                    <h4 class="card-title">
-                                        Rp {{ number_format($totalPemasukan - $totalPengeluaran, 0, ',', '.') }}
+                                    <p class="card-category text-white">Balance</p>
+                                    <h4 class="card-title text-white">
+                                        Rp {{ number_format($balance, 0, ',', '.') }}
                                     </h4>
+                                    @if($balance >= 0)
+                                    @else
+
+                                    @endif
                                 </div>
                             </div>
                         </div>
@@ -105,108 +119,111 @@
             </div>
         </div>
 
-     <div class="row mb-3">
+        {{-- Tombol Tambah Pemasukan --}}
+        <div class="row mb-3">
             <div class="col-md-2">
                 <a href="{{ route('admin.keuangan.createPemasukan') }}" class="btn btn-success btn-sm w-100">
                     <i class="fa fa-plus"></i> Pemasukan
                 </a>
             </div>
-     </div>
-    {{-- Tabel Pemasukan --}}
-    <div class="card mb-4">
-        <div class="card-header">Data Pemasukan</div>
-        <div class="card-body table-responsive">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Tanggal</th>
-                        <th>Keterangan</th>
-                        <th>Jumlah</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($pemasukan as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ \Carbon\Carbon::parse($item->tanggal)->format('d-m-Y') }}</td>
-                        <td>{{ $item->keterangan }}</td>
-                        <td>Rp {{ number_format($item->jumlah, 0, ',', '.') }}</td>
-                        <td>
-                        <div class="form-button-action">
-                            <a href="{{ route('admin.keuangan.editPemasukan', $item->id_pemasukan) }}" class="btn btn-link btn-warning btn-sm" data-bs-toggle="tooltip" title="Edit Pemasukan">
-                                <i class="fa fa-edit fa-lg"></i>
-                            </a>
-                            <form action="{{ route('admin.keuangan.destroyPemasukan', $item->id_pemasukan) }}" method="POST" class="d-inline hapus-data">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-link btn-danger btn-sm btn-hapus" data-bs-toggle="tooltip" title="Hapus Pemasukan">
-                                    <i class="fa fa-times fa-lg"></i>
-                                </button>
-                            </form>
-                        </div>
-
-                        </td>
-                    </tr>
-                    @empty
-                    <tr><td colspan="5" class="text-center">Belum ada pemasukan</td></tr>
-                    @endforelse
-                </tbody>
-            </table>
         </div>
-    </div>
-         <div class="row mb-3">
+
+        {{-- Tabel Pemasukan --}}
+        <div class="card mb-4">
+            <div class="card-header">Data Pemasukan</div>
+            <div class="card-body table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Tanggal</th>
+                            <th>Keterangan</th>
+                            <th>Jumlah</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($pemasukan as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ \Carbon\Carbon::parse($item->tanggal)->format('d-m-Y') }}</td>
+                            <td>{{ $item->keterangan }}</td>
+                            <td>Rp {{ number_format($item->jumlah, 0, ',', '.') }}</td>
+                            <td>
+                                <div class="form-button-action">
+                                    <a href="{{ route('admin.keuangan.editPemasukan', $item->id_pemasukan) }}" class="btn btn-link btn-warning btn-sm" data-bs-toggle="tooltip" title="Edit Pemasukan">
+                                        <i class="fa fa-edit fa-lg"></i>
+                                    </a>
+                                    <form action="{{ route('admin.keuangan.destroyPemasukan', $item->id_pemasukan) }}" method="POST" class="d-inline hapus-data">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-link btn-danger btn-sm btn-hapus" data-bs-toggle="tooltip" title="Hapus Pemasukan">
+                                            <i class="fa fa-times fa-lg"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr><td colspan="5" class="text-center">Belum ada pemasukan</td></tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        {{-- Tombol Tambah Pengeluaran --}}
+        <div class="row mb-3">
             <div class="col-md-2">
                 <a href="{{ route('admin.keuangan.createPengeluaran') }}" class="btn btn-danger btn-sm w-100">
                     <i class="fa fa-plus"></i> Pengeluaran
                 </a>
             </div>
-         </div>
-    {{-- Tabel Pengeluaran --}}
-    <div class="card mb-4">
-        <div class="card-header">Data Pengeluaran</div>
-        <div class="card-body table-responsive">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Tanggal</th>
-                        <th>Keterangan</th>
-                        <th>Jumlah</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($pengeluaran as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ \Carbon\Carbon::parse($item->tanggal)->format('d-m-Y') }}</td>
-                        <td>{{ $item->keterangan }}</td>
-                        <td>Rp {{ number_format($item->jumlah, 0, ',', '.') }}</td>
-                        <td>
-                        <div class="form-button-action">
-                            <a href="{{ route('admin.keuangan.editPengeluaran', $item->id_pengeluaran) }}" class="btn btn-link btn-warning btn-sm" data-bs-toggle="tooltip" title="Edit Pengeluaran">
-                                <i class="fa fa-edit fa-lg"></i>
-                            </a>
-                            <form action="{{ route('admin.keuangan.destroyPengeluaran', $item->id_pengeluaran) }}" method="POST" class="d-inline hapus-data">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-link btn-danger btn-sm btn-hapus" data-bs-toggle="tooltip" title="Hapus Pengeluaran">
-                                    <i class="fa fa-times fa-lg"></i>
-                                </button>
-                            </form>
-                        </div>
+        </div>
 
-                        </td>
-                    </tr>
-                    @empty
-                    <tr><td colspan="5" class="text-center">Belum ada pengeluaran</td></tr>
-                    @endforelse
-                </tbody>
-            </table>
+        {{-- Tabel Pengeluaran --}}
+        <div class="card mb-4">
+            <div class="card-header">Data Pengeluaran</div>
+            <div class="card-body table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Tanggal</th>
+                            <th>Keterangan</th>
+                            <th>Jumlah</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($pengeluaran as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ \Carbon\Carbon::parse($item->tanggal)->format('d-m-Y') }}</td>
+                            <td>{{ $item->keterangan }}</td>
+                            <td>Rp {{ number_format($item->jumlah, 0, ',', '.') }}</td>
+                            <td>
+                                <div class="form-button-action">
+                                    <a href="{{ route('admin.keuangan.editPengeluaran', $item->id_pengeluaran) }}" class="btn btn-link btn-warning btn-sm" data-bs-toggle="tooltip" title="Edit Pengeluaran">
+                                        <i class="fa fa-edit fa-lg"></i>
+                                    </a>
+                                    <form action="{{ route('admin.keuangan.destroyPengeluaran', $item->id_pengeluaran) }}" method="POST" class="d-inline hapus-data">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-link btn-danger btn-sm btn-hapus" data-bs-toggle="tooltip" title="Hapus Pengeluaran">
+                                            <i class="fa fa-times fa-lg"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr><td colspan="5" class="text-center">Belum ada pengeluaran</td></tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-
 </div>
 @endsection
